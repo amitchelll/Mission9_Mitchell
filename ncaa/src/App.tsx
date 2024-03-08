b48755bf2eb8f9ad5a398ca80dd2ddd4b429f194
amitchelll <131500645+amitchelll@users.noreply.github.com>
2024-03-08 16:02:21 -0700

    the whole project

--- ncaa/src/App.tsx
+++ ncaa/src/App.tsx
@@ -1,24 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import collegeInfo from './CollegeBasketballTeams.json';
+
+const styles = {
+  collegeContainer: {
+    backgroundColor: '#f0f0f0',
+    padding: '20px',
+    margin: '10px',
+    borderRadius: '5px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+  },
+  school: {
+    color: '#333',
+    fontSize: '24px',
+    marginBottom: '5px',
+  },
+  mascot: {
+    color: '#666',
+    marginBottom: '5px',
+  },
+  location: {
+    color: '#666',
+  }
+};
+
+
+interface CollegeProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+
+function Welcome() {
+  return (
+    <h1>Colleges in NCAA Basketball</h1>
+  );
+}
+
+
+class Colleges extends React.Component<CollegeProps> {
+  render() {
+    const { school, name, city, state } = this.props;
+    return (
+      <div style={styles.collegeContainer}>
+        <h2 style={styles.school}>{school}</h2>
+        <h3 style={styles.mascot}>Mascot: {name}</h3>
+        <h3 style={styles.location}>Location: {city}, {state}</h3>
+      </div>
+    );
+  }
+}
+
+function CollegeList () {
+  return (
+    <div>
+      {collegeInfo.teams.map((college: CollegeProps) => (
+        <Colleges {...college}/>
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      < Welcome />
+      < CollegeList />
     </div>
   );
 }
